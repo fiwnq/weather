556df92e96b4adb436af078c874b5937e78163c9
AdhamA <Adhamalammuri@gmail.com>
2023-12-09 17:52:27 -0600

    connected the styleing with the api calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import './App.css';
 import Weather from './weather.js'; 
 
@@ -35,6 +35,36 @@ function App() {
       
     </div>
   );
+}
+
+export default App;
+old */
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Weather from './weather.js';
+
+function App() {
+    const [location, setLocation] = useState('Nashville US');
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const newLocation = event.target.elements.inputLocation.value.trim();
+        setLocation(newLocation); 
+    };
+
+    return (
+        <div>
+            <h1>Lipscomb Weather App</h1>
+            <div className="row-1">
+                <form id="query-weather" method="POST" onSubmit={handleSearch}>
+                    <input type="text" className="input-left" id="input-location" name="inputLocation" placeholder="Enter Location" />
+                    <button className="button-right" type="submit"><i className="fa fa-search"></i></button>
+                </form>
+            </div>
+            <Weather location={location} />
+        </div>
+    );
 }
 
 export default App;
--- src/weather.js
+++ src/weather.js
@@ -2,72 +2,90 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Weather({ location }) { 
-  const [currentWeather, setCurrentWeather] = useState({});
-  const [forecast, setForecast] = useState({});
+    const [currentWeather, setCurrentWeather] = useState({});
+    const [forecast, setForecast] = useState([]);
 
-  useEffect(() => {
-    const currentOptions = {
-      method: 'GET',
-      url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-      params: { q: location },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const headers = {
+                    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+                    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+                };
+
+                // First API call for the first three days of forecast
+                const initialForecastResponse = await axios.get('https://weatherapi-com.p.rapidapi.com/forecast.json', {
+                    params: { q: location, days: '3' },
+                    headers: headers
+                });
+                
+                let combinedForecast = [...initialForecastResponse.data.forecast.forecastday];
+
+                // Subsequent calls for each of the next three days
+                for (let i = 1; i <= 3; i++) {
+                    const nextDay = new Date(initialForecastResponse.data.forecast.forecastday[2].date);
+                    nextDay.setDate(nextDay.getDate() + i);
+                    const formattedDate = nextDay.toISOString().split('T')[0];
+
+                    const singleDayForecastResponse = await axios.get('https://weatherapi-com.p.rapidapi.com/forecast.json', {
+                        params: { q: location, dt: formattedDate },
+                        headers: headers
+                    });
+
+                    combinedForecast.push(singleDayForecastResponse.data.forecast.forecastday[0]);
+                }
+
+                setForecast(combinedForecast);
+
+                // Get current weather data
+                const currentWeatherResponse = await axios.get('https://weatherapi-com.p.rapidapi.com/current.json', {
+                    params: { q: location },
+                    headers: headers
+                });
+                setCurrentWeather(currentWeatherResponse.data);
+
+            } catch (error) {
+                console.error('Error fetching weather data:', error);
+            }
+        };
+
+        fetchData();
+    }, [location]);
+
+    const getWeekday = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', { weekday: 'long' });
     };
 
-    const forecastOptions = {
-      method: 'GET',
-      url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-      params: { q: location, days: '3' },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-      }
-    };
+    return (
+        <div>
+            {currentWeather.current && (
+                <div className="current-condition">
+                    <img src={currentWeather.current.condition.icon} alt="Weather Icon" style={{ width: '90px' }} />
+                    <div>
+                        <p className="current-temp">{currentWeather.current.temp_f}°F</p>
+                        <p className="extraInfo1">Precipitation: {currentWeather.current.precip_mm} mm</p>
+                        <p className="extraInfo2">Humidity: {currentWeather.current.humidity}%</p>
+                    </div>
+                </div>
+            )}
 
-    // Make the API call for current weather
-    axios.request(currentOptions)
-      .then(response => {
-        console.log("Weather data:", response.data);
-        setCurrentWeather(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching current weather data:', error);
-      });
-
-    // Make the API call for the forecast
-    axios.request(forecastOptions)
-      .then(response => {
-        console.log("Future data:", response.data);
-        setForecast(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching forecast data:', error);
-      });
-
-  }, [location]); 
-
-  return (
-    <div>
-      <h2>Weather Data</h2>
-      <p>
-        {currentWeather.current ? `Current Temperature: ${currentWeather.current.temp_f}°F` : 'Loading current weather data...'}
-      </p>
-      <h2>Forecast</h2>
-      {forecast.forecast && forecast.forecast.forecastday ? (
-        forecast.forecast.forecastday.map((day, index) => (
-          <div key={index}>
-            <p>Date: {day.date}</p>
-            <p>Max Temperature: {day.day.maxtemp_f}°F</p>
-            <p>Min Temperature: {day.day.mintemp_f}°F</p>
-          </div>
-        ))
-      ) : (
-        <p>Loading forecast data...</p>
-      )}
-    </div>
-  );
+            <div className="forecast-container">
+                {forecast.length > 0 ? (
+                    forecast.map((day, index) => (
+                        <div className="forecast-day" key={index}>
+                            <p>{getWeekday(day.date)}</p>
+                            <img src={day.day.condition.icon} alt="Weather Icon" style={{ width: '50px' }} />
+                            <p>High: {day.day.maxtemp_f}°F</p>
+                            <p>Low: {day.day.mintemp_f}°F</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>Loading forecast data...</p>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Weather;
